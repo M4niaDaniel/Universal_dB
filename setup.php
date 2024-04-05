f9da4081d3cbe037381f7088b4eeedf1e72704c8
M4niaDaniel <148873201+M4niaDaniel@users.noreply.github.com>
2024-04-05 11:20:26 +0200

    Update setup.php

--- setup.php
+++ setup.php
@@ -1,34 +1,58 @@
 <?php
 
-    #display function
-    function displayTables($table, $connection, $addSQL){
-        if(!is_array($table)){
-            $tables = [$table];
-        }else{
-            $tables = $table;
-        }
-        for($i=0;$i<count($tables);$i++){
+class Database{
 
-            $sql = "SELECT * FROM ".$tables[$i].' '.$addSQL;
-            $result = $connection->query($sql);
-            $result2 = $connection->query($sql) ->fetch_assoc();
+    private $hostname;
+    private $username;
+    private $password;
+    private $database;
+        function __construct($hostname, $username){
 
-            echo "<table><tr>";
-            foreach ($result2 as $key => $value) {
-                echo "<th>".$key."</th>";
+            #server connection
+
+            $this->hostname = $hostname;
+            $this->username = $username;
+            $this->hostname = $hostname;
+            $this->hostname = $hostname;
+
+            $connection =  new mysqli($this->hostname, $username, $password, $database);
+
+            if ($connection -> connect_error) {
+                die("Connection failed: " . $connection -> connect_error);
+            }else{
+                echo "<script>console.log('Connection granted')</script>";
             }
-            echo "</tr>";
-            while($row = $result->fetch_array(MYSQLI_NUM))
-            {   
-                echo "<tr>";
-                for($j=0;$j < count($row);$j++)
-                {
-                    echo "<td>".$row[$j]."</td>";
+
+            #display tables
+            function displayTables($table, $connection, $addSQL){
+                if(!is_array($table)){
+                    $tables = [$table];
+                }else{
+                    $tables = $table;
                 }
-                echo "</tr>";
+                for($i=0;$i<count($tables);$i++){
+        
+                    $sql = "SELECT * FROM ".$tables[$i].' '.$addSQL;
+                    $result = $connection->query($sql);
+                    $result2 = $connection->query($sql) ->fetch_assoc();
+        
+                    echo "<table><tr>";
+                    foreach ($result2 as $key => $value) {
+                        echo "<th>".$key."</th>";
+                    }
+                    echo "</tr>";
+                    while($row = $result->fetch_array(MYSQLI_NUM))
+                    {   
+                        echo "<tr>";
+                        for($j=0;$j < count($row);$j++)
+                        {
+                            echo "<td>".$row[$j]."</td>";
+                        }
+                        echo "</tr>";
+                    }
+                    echo "</table>";
+                }
             }
-            echo "</table>";
         }
     }
-
 ?>
